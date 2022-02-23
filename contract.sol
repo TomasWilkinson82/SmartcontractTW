c57c297132e8e82a7df0e5477f0a8411bea78c43
TomasWilkinson82 <99285594+TomasWilkinson82@users.noreply.github.com>
2022-02-23 17:13:00 +0000

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -2,13 +2,22 @@ pragma solidity ^0.8.0;
 
 contract Greeter {
   string public greeting;
-
+  string public name;
+  string public studentNumber;
+  uint public count;
+  
   constructor() public {
-    greeting = 'Hello';
+    name = 'tomas wilkinson';
+    studentNumber = '10033089';
+    greeting = 'Hello from the contract deployed be ' + name + ' with student number ' + studentNumber ';
+    count = 0;
   }
 
-  function setGreeting(string memory _greeting) public {
-    greeting = _greeting;
+  function setGreeting(string memory _name, string memory _studentNumber) public {
+    name = _name;
+    studentNumber = _studentNumber;
+    greeting = 'Hello from the contract deployed be ' + name + ' with student number ' + studentNumber ';
+    count++
   }
 
   function greet() view public returns (string memory) {
